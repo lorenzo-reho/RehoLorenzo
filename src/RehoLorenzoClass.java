920be766e8740e5b11cc2954773e456aa1a17798
RehoLorenzo <reho@gmail.com>
2022-05-04 11:33:57 +0200

    versione 0: errata

---
+++ src/RehoLorenzoClass.java
@@ -0,0 +1,47 @@
+/*
+ * RehoLorenzoClass
+ * 
+ * @author <b>Reho Lorenzo</b> <u>4CI</u> 04/05/2022
+ * @version 1.0
+ * 
+ * <i>04/05/2022</i>
+ * 
+ * */
+public class RehoLorenzoClass {
+	
+	/*
+	 * Costruttore di RehoLorenzoClass
+	 * 
+	 * */
+	
+	public RehoLorenzoClass() {
+		
+	
+	}
+	
+	/*
+	 * 
+	 * Metodo che calcola la sommatoria da f a 0
+	 * 
+	 * @param f valore max della sommatoria
+	 * @return risultato della sommatoria
+	 * 
+	 * */
+	public int rehoMet(int f) {
+		int r = 1;
+		for(int i = f; i < 0; i++)
+			r *= i;
+		return r;
+	}
+	
+	/*
+	 * 
+	 * Metodo main
+	 * 
+	 * */
+	
+	public static void main(String[] args) {
+		RehoLorenzoClass rehoClass = new RehoLorenzoClass();
+		System.out.println(rehoClass.rehoMet(5));
+	}
+}
